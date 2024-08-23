266c24f4dbc68b96b63957f4d430d0494576fe54
max-t-smith <maxtuerkheimersmith@gmail.com>
2024-08-22 22:41:40 -0500

    first commit

---
+++ script/main.js
@@ -0,0 +1,12 @@
+function loadMenu()
+{
+    document.getElementById("navbar").style.display = "block";
+    document.getElementById("menu_icon").style.display ="none";
+}
+
+function hideMenu()
+{
+    document.getElementById("navbar").style.display = "none";
+    document.getElementById("menu_icon").style.display ="inline";
+}
+
