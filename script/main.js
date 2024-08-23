efec6bdad7b4bd49a0be249447e728e85fd4c579
max-t-smith <maxtuerkheimersmith@gmail.com>
2024-08-23 11:14:35 -0500

    abandoned ship

--- script/main.js
+++ script/main.js
@@ -9,10 +9,3 @@ function hideMenu()
     document.getElementById("navbar").style.display = "none";
     document.getElementById("menu_icon").style.display ="inline";
 }
-
-function sendCCEmail()
-{
-    document.getElementById("_cc").value = document.getElementById("email").value;
-    document.querySelector('form').submit();
-}
-
